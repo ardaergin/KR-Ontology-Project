80a13e1c9b76f9337497acf8323be39f7e94b5bc
Evalunaellinger <78860776+Evalunaellinger@users.noreply.github.com>
2023-11-16 21:03:07 +0100

    Added reasoner.py file, adjusted kr1_poke.rdf - it now has 20 classes, added the example files

---
+++ reasoner.py
@@ -0,0 +1,16 @@
+from py4j.java_gateway import JavaGateway
+
+# connect to the java gateway of dl4python
+gateway = JavaGateway()
+
+# get a parser from OWL files to DL ontologies
+parser = gateway.getOWLParser()
+
+# get a formatter to print in nice DL format
+formatter = gateway.getSimpleDLFormatter()
+
+print("Loading the ontology...")
+# load an ontology from a file
+ontology = parser.parseFile("KR1_POKE.rdf")
+
+print(ontology)
